75ae954bda8a56dac1e00ad0ad677853807118d7
Greg K <gkreisman_cep@qq.com>
2024-05-16 08:47:34 +0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var capture;
+let cre
+function preload(){
+    cre = loadImage("ngl.jpg")
+}
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }    
+    //video: {
+      //facingMode: "user"
+    //} 
+  };
+  capture = createCapture(constraints);
+  
+  capture.hide();
+  cre.resize(200,0)
+}
+
+
+function draw() {
+ image(capture, 0, 0); 
+ image(cre,width/2,height/2)
+}
